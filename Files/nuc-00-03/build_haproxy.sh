b6b82d4cf909e19fad1d887dd3fd0968c601d5b2
James Radtke <cloudxabide@gmail.com>
2026-02-08 15:09:46 -0500

    generic update

--- Files/nuc-00-03/build_haproxy.sh
+++ Files/nuc-00-03/build_haproxy.sh
@@ -11,18 +11,26 @@ SUSEConnect --product sle-module-server-applications/15.7/x86_64
 #SUSEConnect --product sle-ha/15.7/x86_64 -r (add reg code for HA Extension)
 }
 
+# Open Ports
+TCP_PORTS="9000 80 443 6443"
+for PORT in $TCP_PORTS
+do 
+  firewall-cmd --permanent --add-port=${PORT}/tcp
+done
+firewll-cmd --reload
+
 # using Keepalived for floating/VIP (and to future proof)
 zypper -n in haproxy keepalived
 
 # Allow keepalive to attach before interface is up/available
 echo "net.ipv4.ip_nonlocal_bind = 1" | sudo tee -a /etc/sysctl.d/20_keepalive.conf
 mv /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.orig
-curl -o  /etc/keepalived/keepalived.conf https://raw.githubusercontent.com/jradtke-suse/kubernerdes.lab/refs/heads/main/Files/etc_keepalived_keepalived-$(uname -n).conf 
+curl -o  /etc/keepalived/keepalived.conf https://raw.githubusercontent.com/jradtke-rgs/enclave.kubernerdes.com/refs/heads/main/Files/nuc-00-03/etc_keepalived_keepalived.conf
 sdiff /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.orig
 sudo systemctl enable keepalived --now
 sleep 15; ip a s
 
 cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.$(uuidgen | tr -d '-' | head -c 6)
-curl -o /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/jradtke-suse/kubernerdes.lab/refs/heads/main/Files/etc_haproxy_haproxy-$(uname -n).cfg
+curl -o /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/jradtke-rgs/enclave.kubernerdes.com/refs/heads/main/Files/nuc-00-03/etc_haproxy_haproxy.cfg
 sudo systemctl enable haproxy --now
 
