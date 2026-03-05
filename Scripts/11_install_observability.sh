d0887f5eb8742117af90ebbff4ef358bc0e59212
James Radtke <cloudxabide@gmail.com>
2026-03-05 13:23:56 -0500

    generic update

--- Scripts/11_install_observability.sh
+++ Scripts/11_install_observability.sh
@@ -1,32 +1,16 @@
 # This "script" was not intended to be run as a script, and instead cut-and-paste the pieces (hence no #!/bin/sh at the top ;-_
 # Reference: https://observabilitymanager.docs.observability.com/how-to-guides/new-user-guides/kubernetes-cluster-setup/k3s-for-observability
 
-scp 10.10.12.221:.kube/config .kube/enclave-observability.kubeconfig
-echo "Note: you should update the cluster: name: value"
-export KUBECONFIG=~/.kube/enclave-observability.kubeconfig
+# you need to retrieve the KUBECONFIG from Rancher Manager
+# save it as ~/.kube/enclave-observability.kubeconfig
 chmod 0664 $KUBECONFIG
+export KUBECONFIG=~/.kube/enclave-observability.kubeconfig
 scp $KUBECONFIG 10.10.12.10:/srv/www/.kube/
-
-#######################################
-# Install Local Path Provisioner for Storage
-#######################################
-# NOTE: The RKE2 VM disks are already backed by Harvester Longhorn (replica=3),
-#       so we use local-path-provisioner to avoid redundant replication.
-#       Do NOT install standalone Longhorn or the Harvester CSI driver here.
-LOCAL_PATH_VERSION=v0.0.30
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/${LOCAL_PATH_VERSION}/deploy/local-path-storage.yaml
-kubectl wait --for=condition=available deployment/local-path-provisioner -n local-path-storage --timeout=60s
-
-# Set local-path as the default StorageClass
-kubectl patch sc local-path -p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-
-# Verify
-kubectl get sc
 
 #######################################
 # Install Cert Manager
 #######################################
-CERTMGR_VERSION=v1.18.0
+CERTMGR_VERSION=v1.19.4
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CERTMGR_VERSION}/cert-manager.crds.yaml
 
 helm repo add jetstack https://charts.jetstack.io
@@ -38,6 +22,7 @@ helm repo update
 echo "Installing SUSE Observability (StackState)..."
 
 # I do this in a separate/well-known directory - not necessary
+mv ~/Developer/Projects/observability.enclave.kubernerdes.com ~/Developer/Projects/observability.enclave.kubernerdes.com-$(date +%F)
 mkdir -p ~/Developer/Projects/observability.enclave.kubernerdes.com; cd $_
 
 # Add the SUSE Observability Helm Repo
@@ -47,7 +32,7 @@ helm repo update
 # Create template files
 export VALUES_DIR=.
 helm template \
-  --set license='$O11Y_LICENSE' \
+  --set license="$O11Y_LICENSE" \
   --set rancherUrl='https://rancher.enclave.kubernerdes.com' \
   --set baseUrl='https://observability.enclave.kubernerdes.com' \
   --set sizing.profile='10-nonha' \
@@ -65,9 +50,16 @@ helm upgrade --install \
     suse-observability/suse-observability
 
 kubectl get all -n suse-observability
+
+echo "Note: you are going to see a LOT of scary warnings while things spin up.  You can likely ignore them.
+echo "      This will take 15-20 minutes before things are working and stabilized"
+kubectl get pods -n suse-observability -w
+
 grep 'admin password' $(find $HOME -name baseConfig_values.yaml)
 
-# Create ingress using traefik for O11y (for K3s)
+# Create ingress using traefik for O11y (for K3s or RKE2)
+case $(kubectl version -o json | jq -r '.serverVersion.gitVersion') in 
+  *k3s*)
 cat << EOF > suse-observability-ingress.yaml
 ---
 apiVersion: networking.k8s.io/v1
@@ -95,9 +87,8 @@ spec:
   - hosts:
     - observability.enclave.kubernerdes.com
 EOF
-kubectl apply -f ./suse-observability-ingress.yaml
-
-# Create ingress using traefik for O11y (for RKE2)
+  ;;
+  *rke2*)
 cat << EOF > suse-observability-ingress.yaml
 ---
 apiVersion: networking.k8s.io/v1
@@ -119,6 +110,8 @@ spec:
             port:
               number: 8080
 EOF
+  ;;
+esac
 kubectl apply -f ./suse-observability-ingress.yaml
 
 exit 0
