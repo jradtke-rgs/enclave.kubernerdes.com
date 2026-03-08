4d7051b9df935bf6fdf2d1eb12f32010fa807981
James Radtke <cloudxabide@gmail.com>
2026-03-07 19:36:22 -0500

    Adding Security components

---
+++ Scripts/20_install_RGS_security.sh
@@ -0,0 +1,75 @@
+# This "script" was not intended to be run as a script, and instead cut-and-paste the pieces (hence no #!/bin/sh at the top ;-)
+# Reference: https://open-docs.neuvector.com/deploying/kubernetes
+# Reference: https://ranchermanager.docs.rancher.com/integrations-in-rancher/neuvector
+
+# SUSE Security (NeuVector) on the "applications" cluster
+# Nodes: 10.10.15.43 10.10.15.44 10.10.15.45
+# SSH:   ssh -i ~/.ssh/id_rsa-kubernerdes sles@10.10.15.43
+
+export KUBECONFIG=~/.kube/enclave-applications.kubeconfig
+
+#######################################
+# Install SUSE Security (NeuVector)
+#######################################
+
+# Add the NeuVector Helm repo
+helm repo add neuvector https://neuvector.github.io/neuvector-helm/
+helm repo update
+
+# Create namespace
+kubectl create namespace cattle-neuvector-system
+
+# Install NeuVector via Helm
+helm upgrade --install neuvector neuvector/core \
+  --namespace cattle-neuvector-system \
+  --set manager.svc.type=ClusterIP \
+  --set controller.replicas=3 \
+  --set cve.scanner.replicas=2 \
+  --set controller.pvc.enabled=false \
+  --set k3s.enabled=false \
+  --set manager.ingress.enabled=false \
+  --set global.cattle.url=https://rancher.enclave.kubernerdes.com
+
+# Wait for pods to be ready
+kubectl get pods -n cattle-neuvector-system -w
+
+#######################################
+# Create Ingress for NeuVector Manager
+#######################################
+cat << EOF > neuvector-ingress.yaml
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: neuvector-manager
+  namespace: cattle-neuvector-system
+  annotations:
+    nginx.ingress.kubernetes.io/backend-protocol: "HTTPS"
+spec:
+  ingressClassName: nginx
+  rules:
+    - host: neuvector.applications.enclave.kubernerdes.com
+      http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: neuvector-service-webui
+                port:
+                  number: 8443
+  tls:
+    - hosts:
+        - neuvector.applications.enclave.kubernerdes.com
+EOF
+
+kubectl apply -f neuvector-ingress.yaml
+
+# Verify ingress
+kubectl get ingress -n cattle-neuvector-system
+
+# Retrieve bootstrap password
+echo "NeuVector UI: https://neuvector.applications.enclave.kubernerdes.com"
+echo "Bootstrap password: $(kubectl get secret --namespace cattle-neuvector-system neuvector-bootstrap-secret -o go-template='{{ .data.bootstrapPassword|base64decode}}{{ "\n" }}')"
+
+exit 0
---
+++ Scripts/81_cve_comparison_example.sh
@@ -0,0 +1,96 @@
+# This "script" was not intended to be run as a script, and instead cut-and-paste the pieces (hence no #!/bin/sh at the top ;-)
+# Reference: https://open-docs.neuvector.com/deploying/kubernetes
+# Reference: https://ranchermanager.docs.rancher.com/integrations-in-rancher/neuvector
+# Reference: https://rancherfederal.github.io/carbide-docs/docs/registry-docs/downloading-images
+
+# SUSE Security (NeuVector) on the "applications" cluster
+# SSH:   ssh -i ~/.ssh/id_rsa-kubernerdes sles@<IP>
+
+export KUBECONFIG=~/.kube/enclave-applications.kubeconfig
+
+
+#######################################
+# CVE Comparison: Community vs Carbide
+#######################################
+# Deploy Rancher pods to compare community vs Carbide (hardened) images in NeuVector.
+# The Carbide registry (rgcrprod.azurecr.us) provides hardened Rancher ecosystem
+# images with fewer CVEs. See carbide-images.txt for the full list:
+#   https://github.com/rancherfederal/carbide-releases/blob/main/carbide-images.txt
+
+RANCHER_VERSION="v2.13.3"
+
+# #########################################
+## COMMUNITY CONTAINER
+# #########################################
+NAMESPACE=cvet-rancher-comm
+kubectl create namespace ${NAMESPACE}
+
+# Community Rancher image (from Docker Hub)
+cat << EOF | kubectl apply -f -
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: rancher-community
+  namespace: ${NAMESPACE}
+  labels:
+    app: rancher
+    source: community
+spec:
+  containers:
+  - name: rancher
+    image: docker.io/rancher/rancher:${RANCHER_VERSION}
+    command: ["sleep", "infinity"]
+EOF
+
+kubectl wait --for=condition=Ready pod/rancher-community -n ${NAMESPACE} --timeout=120s
+kubectl get pods -n ${NAMESPACE}
+
+# #########################################
+## CARBIDE CONTAINER
+# #########################################
+NAMESPACE=cvet-rancher-carb
+kubectl create namespace ${NAMESPACE}
+
+#   Carbide credentials must exist in ~/.hauler/credentials with:
+#     HAULER_USER, HAULER_PASSWORD, HAULER_SOURCE_REPO_URL
+#
+source ~/.hauler/credentials
+hauler login "$HAULER_SOURCE_REPO_URL" -u "$HAULER_USER" -p "$HAULER_PASSWORD"
+
+# Create imagePullSecret so kubelet can pull from the Carbide registry
+kubectl create secret docker-registry carbide-registry \
+  --namespace ${NAMESPACE} \
+  --docker-server="$HAULER_SOURCE_REPO_URL" \
+  --docker-username="$HAULER_USER" \
+  --docker-password="$HAULER_PASSWORD"
+
+# Carbide Rancher image (hardened, from rgcrprod.azurecr.us)
+cat << EOF | kubectl apply -f -
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: rancher-carbide
+  namespace: ${NAMESPACE}
+  labels:
+    app: rancher
+    source: carbide
+spec:
+  imagePullSecrets:
+  - name: carbide-registry
+  containers:
+  - name: rancher
+    image: rgcrprod.azurecr.us/rancher/rancher:${RANCHER_VERSION}
+    command: ["sleep", "infinity"]
+EOF
+
+kubectl wait --for=condition=Ready pod/rancher-carbide -n ${NAMESPACE} --timeout=120s
+kubectl get pods -n ${NAMESPACE}
+
+# #########################################
+## CLEANUP (when done with the comparison)
+# #########################################
+# kubectl delete namespace cvet-rancher-comm cvet-rancher-carb
+
+exit 0
